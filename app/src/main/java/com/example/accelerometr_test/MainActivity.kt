cd8b9934275db5b2c13125106506d4259ed32805
Alexandr Prokopov <alexprokopov@gmail.com>
2023-05-04 15:19:59 +0300

    dsadasdas

--- app/src/main/java/com/example/accelerometr_test/MainActivity.kt
+++ app/src/main/java/com/example/accelerometr_test/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.accelerometr_test
 
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.ads.AdRequest
@@ -7,14 +8,59 @@ import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.admanager.AdManagerAdView
 import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import kotlin.math.sqrt
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mAdView : AdView
-
+    private lateinit var locationManager: LocationManager
+    private lateinit var sensorManager: SensorManager
+    private val REQUEST_LOCATION_PERMISSION = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView3)
+        textView.text = "Checking permission"
+
+        val locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                val gpsSpeed = location.speed
+//                updateVelocity(gpsSpeed)
+            }
+
+            override fun onProviderEnabled(provider: String) {}
+            override fun onProviderDisabled(provider: String) {}
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+        }
+
+        while (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            textView.text = "Requesting permission"
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        }
+        textView.text = "Permission granted!"
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
         MobileAds.initialize(this) {}
 
@@ -23,3 +69,4 @@ class MainActivity : AppCompatActivity() {
         mAdView.loadAd(adRequest)
     }
 }
+
