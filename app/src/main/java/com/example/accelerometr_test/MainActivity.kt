d3e10960426f206deab164672e8311f353037bfc
Alexandr Prokopov <alexprokopov@gmail.com>
2023-05-04 17:06:06 +0300

    321

--- app/src/main/java/com/example/accelerometr_test/MainActivity.kt
+++ app/src/main/java/com/example/accelerometr_test/MainActivity.kt
@@ -33,6 +33,7 @@ class MainActivity : AppCompatActivity() {
     private var PERM_FINE_LOCATION = false
     private var PERM_COARSE_LOCATION = false
     private var PERM_OK = false
+    private var Requested = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,31 +41,85 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.textView3)
         textView.text = "Hello"
 
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
+        val locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                val gpsSpeed = location.speed
+//                updateVelocity(gpsSpeed)
+            }
+
+            override fun onProviderEnabled(provider: String) {}
+            override fun onProviderDisabled(provider: String) {}
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+        }
+        var tryes = 0
+        while (PERM_OK !== true)
+        {
+            checkPermissions()
+            Thread.sleep(1000)
+            tryes++
+            if(tryes > 20) break
+        }
+        if (PERM_OK) {
+            Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show()
+            textView.text = "Perm OK!"
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+        } else {
+            Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show()
+            Thread.sleep(1000)
+            finishAffinity()
+        }
+    }
+
+    fun checkPermissions() {
         if (ContextCompat.checkSelfPermission(this@MainActivity,
                 Manifest.permission.ACCESS_FINE_LOCATION) !==
             PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                ActivityCompat.requestPermissions(this@MainActivity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-            } else {
-                ActivityCompat.requestPermissions(this@MainActivity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            if(Requested !== true) {
+                Toast.makeText(this, "Waiting for permissions...", Toast.LENGTH_SHORT).show()
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this@MainActivity,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
+                    )
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
+                    )
+                }
             }
         }
         if (ContextCompat.checkSelfPermission(this@MainActivity,
                 Manifest.permission.ACCESS_COARSE_LOCATION) !==
             PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                ActivityCompat.requestPermissions(this@MainActivity,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
-            } else {
-                ActivityCompat.requestPermissions(this@MainActivity,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+            if (Requested !== true) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this@MainActivity,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1
+                    )
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1
+                    )
+                }
             }
         }
         if (ContextCompat.checkSelfPermission(this@MainActivity,
@@ -77,49 +132,13 @@ class MainActivity : AppCompatActivity() {
             PackageManager.PERMISSION_GRANTED) {
             PERM_COARSE_LOCATION = true
         }
-
-        val locationListener = object : LocationListener {
-            override fun onLocationChanged(location: Location) {
-                val gpsSpeed = location.speed
-//                updateVelocity(gpsSpeed)
-            }
-
-            override fun onProviderEnabled(provider: String) {}
-            override fun onProviderDisabled(provider: String) {}
-            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-        }
-
         if (PERM_OK == false) {
             if (PERM_COARSE_LOCATION !== true && PERM_FINE_LOCATION !== true) {
-                Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show()
-                textView.text = "Permission denied"
             } else {
-                textView.text = "Permission Ok"
-                locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    0,
-                    0f,
-                    locationListener
-                )
                 PERM_OK = true
             }
         }
-
-        MobileAds.initialize(this) {}
-        mAdView = findViewById(R.id.adView)
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.textView3)
-        if (PERM_OK) {
-            textView.text = "Permission Ok"
-        } else {
-            textView.text = "Permission denied"
-        }
+        Requested = true
     }
 
 
