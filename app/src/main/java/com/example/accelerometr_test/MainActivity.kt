fb8cf189733ffabf07d9239f252b41c95bded282
Alexandr Prokopov <alexprokopov@gmail.com>
2023-05-05 13:58:57 +0300

    add buttons

--- app/src/main/java/com/example/accelerometr_test/MainActivity.kt
+++ app/src/main/java/com/example/accelerometr_test/MainActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.ads.admanager.AdManagerAdView
 import com.google.android.gms.ads.admanager.AdManagerAdRequest
 import android.Manifest
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -18,6 +19,7 @@ import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContentProviderCompat.requireContext
@@ -32,13 +34,53 @@ class MainActivity : AppCompatActivity() {
     private lateinit var locationManager: LocationManager
     private lateinit var sensorManager: SensorManager
     private lateinit var textView: TextView
-
+    private lateinit var StartButton: Button
+    private lateinit var ResetButton: Button
+    private lateinit var PauseButton: Button
+    private var WORKING = false
+    private var ONPAUSE = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        WORKING = false
+        ONPAUSE = false
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         textView = findViewById(R.id.textView3)
-        textView.text = "Hello"
+        StartButton = findViewById(R.id.startButton)
+        ResetButton = findViewById(R.id.resetButton)
+        PauseButton = findViewById(R.id.pauseButton)
+        StartButton.setEnabled(true)
+        ResetButton.setEnabled(false)
+        PauseButton.setEnabled(false)
+
+        StartButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Start meassuring.", Toast.LENGTH_SHORT).show()
+            WORKING = !WORKING
+            StartButton.setEnabled(!WORKING)
+            ResetButton.setEnabled(WORKING)
+            PauseButton.setEnabled(WORKING)
+        }
+        ResetButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Reset meassuring.", Toast.LENGTH_SHORT).show()
+            WORKING = false
+            ONPAUSE = false
+            PauseButton.text = "Pause"
+            StartButton.setEnabled(!WORKING)
+            ResetButton.setEnabled(WORKING)
+            PauseButton.setEnabled(WORKING)
+        }
+        PauseButton.setOnClickListener {
+
+            ONPAUSE = !ONPAUSE
+            if (ONPAUSE) {
+                PauseButton.text = "Resume"
+                Toast.makeText(this@MainActivity, "Resume meassuring.", Toast.LENGTH_SHORT).show()
+            } else {
+                PauseButton.text = "Pause"
+                Toast.makeText(this@MainActivity, "Pause meassuring.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         MobileAds.initialize(this) {}
         mAdView = findViewById(R.id.adView)
